c04b5c21bb6033fb8997ad77d9fff2e92efb0621
artyom.abrahamyan <artyom.abrahamyan@beaxy.com>
2019-11-04 18:05:43 +0400

    Fix is_background, add waitpid

--- shell.c
+++ shell.c
@@ -2,6 +2,8 @@
 #include <sys/types.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 void show_prompt(uid_t current_usr_id) {
   if (current_usr_id == 0) {
@@ -12,7 +14,7 @@ void show_prompt(uid_t current_usr_id) {
   }
 }
 
-void read(char * argv[], int * is_background) {
+void readInput(char * argv[], int * is_background) {
   const int inputsize = 256;
   char str[inputsize];
   fgets(str, inputsize, stdin);
@@ -35,28 +37,30 @@ void read(char * argv[], int * is_background) {
 
 int main() {
   uid_t id = getuid();
-  int is_background = 0;
+  int status = 0;
 
   while(1){
-  show_prompt(id);
+	  show_prompt(id);
+	  
+	  int is_background = 0;
 
-  char * argv[10];
-  read(argv, & is_background);
+	  char * argv[10];
+	  readInput(argv, &is_background);
 
-  int pid = fork();
+	  int pid = fork();
 
-  if (pid < 0) {
-    fprintf(stderr, "Fork Failed");
-    return 1;
-  }
-  if (pid > 0 && is_background == 0) {
-    wait();
-  } 
-  else if (pid > 0 && is_background == 1) {}
-  else {
-    execvp(argv[0], argv);
-    fprintf(stderr, "Unable to execute bash command", 30);
-  }
+	  if (pid < 0) {
+		fprintf(stderr, "Fork Failed");
+		return 1;
+	  }
+	  if (pid > 0 && is_background == 0) {
+		waitpid(pid, &status, 0);
+	  } 
+	  else if (pid > 0 && is_background == 1) {}
+	  else {
+		execvp(argv[0], argv);
+		fprintf(stderr, "Unable to execute bash command\n");
+	  }
   }
 
   return 0;
